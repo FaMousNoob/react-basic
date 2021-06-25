bba925d71d77e0ed9fd913ade6787eb3803eed2b
P.Tao <ngobaophuongtao@gmail.com>
2021-06-25 11:33:51 +0700

    learning react

---
+++ src/demoComponents/stateLessComponents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function stateLessComponent() {
+  return (
+    <div>
+      <h1>this is from stateLessComponents js</h1>
+    </div>
+  );
+}
+
+export default stateLessComponent;
---
+++ src/example-div-component/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './header';
+import Carousel from './carousel';
+import ProductList from './productList';
+import Ads from './ads';
+
+class ExampleDivComponent extends Component {
+  render() {
+    return <div>
+        <Header />
+        <Carousel />
+        <ProductList />
+        <Ads />
+    </div>
+  }
+}
+
+export default ExampleDivComponent;
